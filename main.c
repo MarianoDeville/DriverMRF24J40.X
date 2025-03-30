ad35820ef8ba8bae10e34466acdc3bbebe8ebe89
Mariano Deville <mariano_deville@hotmail.com>
2025-03-30 09:01:40 -0300

    prueba

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ void main(void) {
     BoardInit();
     MRF24J40Init();
     
-    SetMensajeSalida("hola mundo. carajo");
+    SetMensajeSalida("hola mundo. carajo, otra vez");
     
     while(1){
         
